70ad7e6c4aa0f9d50e322815ffbf8aa9a754c8a1
mdmominul-310 <mdmominulislam310@gmail.com>
2024-05-31 20:04:26 +0600

    contact number validation

--- src/RegistrationDetails.java
+++ src/RegistrationDetails.java
@@ -1,7 +1,8 @@
 
-
 public class RegistrationDetails {
-	static String username ;
+	static String username;
 	static String password;
+	static String contactNumber;
+	static String email;
 }
-//add email 
+// add email
